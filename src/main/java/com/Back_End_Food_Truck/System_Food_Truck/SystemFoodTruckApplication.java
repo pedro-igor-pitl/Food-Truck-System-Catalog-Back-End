f1fdafe757f101ea06ff5a2fe41aecd2769ec38b
Pedro Igor <pedro.igor.estagio@bnel.com.br>
2025-09-22 12:26:04 -0300

    inicializacao-system-back-end

--- src/main/java/com/Back_End_Food_Truck/System_Food_Truck/SystemFoodTruckApplication.java
+++ src/main/java/com/Back_End_Food_Truck/System_Food_Truck/SystemFoodTruckApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SystemFoodTruckApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SystemFoodTruckApplication.class, args);
 	}
