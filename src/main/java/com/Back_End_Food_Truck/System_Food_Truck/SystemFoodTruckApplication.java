2f5f83719fd8006f089cad89a85eb3f6cce93739
Pedro Igor <pedro.igor.estagio@bnel.com.br>
2025-09-22 12:19:47 -0300

    Inicialização do Projeto

---
+++ src/main/java/com/Back_End_Food_Truck/System_Food_Truck/SystemFoodTruckApplication.java
@@ -0,0 +1,13 @@
+package com.Back_End_Food_Truck.System_Food_Truck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemFoodTruckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemFoodTruckApplication.class, args);
+	}
+
+}
